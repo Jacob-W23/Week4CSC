3aa59b8ae07e6c71639ad11c14aa6df84f2afa1d
Jacob <vb2553yq@go.minnstate.edu>
2022-02-07 18:00:17 -0600

    First Push of Week 3 Project

---
+++ Willprecht_Week3/Controllers/Willprecht_Week3.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Willprecht_Week3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Willprecht_Week3 : ControllerBase
+    {
+
+        [HttpPost(Name = "GetWeatherForecast")]
+        public ActionResult<List<string>> IntList(List<int> inputList)
+        {
+            List<string> rlist = new List<string>();
+            List<int> newList = new List<int>();
+            int counter = 0;
+
+            if (inputList.Count > 0)
+            {
+                inputList.Sort();
+
+                foreach (var val in inputList)
+                {
+                    if (counter > 0)
+                    {
+                        counter++;
+
+                        newList.Add(val);
+
+                        double avg = newList.Average();
+
+                        double sum = newList.Sum(d => Math.Pow(d - avg, 2));
+
+                        double stdDev = Math.Sqrt((sum) / (newList.Count - 1));
+
+                        rlist.Add("Elements: " + counter + " Current Standard Deviation: " + stdDev);
+                    }
+                    else
+                    {
+                        counter++;
+                        newList.Add(val);
+                        rlist.Add("List is too small");
+                    }
+
+                }
+            } else
+            {
+                rlist.Add("Please make sure you are entering numbers into the input.");
+            }
+            return rlist;
+        }
+    }
+}
